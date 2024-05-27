0e1efe0e608ef321bac8d62bf1a6aa76ff8f1a93
nguyenkyky <scholesyy30@gmail.com>
2024-05-27 18:28:40 +0700

    sua lai giao dien

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -52,14 +52,14 @@ function Register(props) {
           </div>
         </div>
       </div>
-      <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+      <div className=" px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
         <h2
           className="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
               xl:text-bold"
         >
           Đăng ký
         </h2>
-        <div className="mt-12">
+        <div className="mt-8">
           <div>
             <div>
               <div className="text-sm font-bold text-gray-700 tracking-wide">
@@ -71,7 +71,7 @@ function Register(props) {
                 className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500"
               />
             </div>
-            <div>
+            <div className="mt-8">
               <div className="text-sm font-bold text-gray-700 tracking-wide">
                 Họ tên
               </div>
